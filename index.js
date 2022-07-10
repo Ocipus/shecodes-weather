47bf4811ea472c3df3019a355b77f090ea0d7b68
Ocipus <106868718+Ocipus@users.noreply.github.com>
2022-07-10 17:31:53 +0200

    new js tests

--- index.js
+++ index.js
@@ -10,77 +10,67 @@ function formatDate(timestamp) {
   }
   let days = [
     "Sunday",
-    " Monday",
+    "Monday",
     "Tuesday",
     "Wednesday",
     "Thursday",
     "Friday",
     "Saturday",
   ];
+
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayTemperature(response) {
-  let tempMin = document.querySelector("#temp-min");
-let tempMax = document.querySelector("#temp-max")
+  celsiusTemperature = response.data.main.temp;
+
   let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#weather-description");
-  let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind");
-  let dateElement = document.querySelector("#date-time");
-  let iconElement = document.querySelector("#icon");
-
-  celsiusTemp = response.data.main.temp;
-
-  tempMin.innerHTML = Math.round(celsiusTemp);
-  tempMax.innerHTML = Math.round(celsiusTemp);
   cityElement.innerHTML = response.data.name;
-  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let dateElement = document.querySelector("#date-time");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
+  let tempMin = document.querySelector("#temp-min");
+  tempMin.innerHTML = Math.round(response.data.main.temp_min);
+
+  let tempMax = document.querySelector("#temp-max");
+  tempMax.innerHTML = Math.round(response.data.main.temp_max);
+
+  let descriptionElement = document.querySelector("#weather-description");
+  descriptionElement.innerHTML = response.data.weather[0].main;
+
+  let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+}
+
 function search(city) {
-  let apiKey = "37c9014ec42aa6b4e9bea13d45c47a71";
-  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-  let apiUrl = `${apiEndpoint}?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeather);
+  let apiKey = "fd8d1409866553a493730aeed9a355b9";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
 }
 
 function handleSubmit(event) {
   event.preventDefault();
-  let cityElement = document.querySelector("#city");
-  search(cityElement.value);
-  }
-
-function fahrenheitTemp(event) {
-  event.preventDefault();
-  let tempMin = document.querySelector("#temp-min");
-  let tempMax = document.querySelector("#temp-max");
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
-  tempMin.innerHTML = Math.round(fahrenheitTemp);
-  tempMax.innerHTML = Math.round(fahrenheitTemp);
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
 }
 
-function celsiusTemperature(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  let tempMin = document.querySelector("#temp-max");
-  tempMin.innerHTML = Math.round(celsiusTemp);
-  let tempMax = document.querySelector("#temp-min");
-  tempMax.innerHTML = Math.round(celsiusTemp);
-}
-
-let celsiusTemp = null;
-
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", handleSubmit);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", fahrenheitTemp);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", celsiusTemperature);
