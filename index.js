d96848f0fa8e2977507a88d2bad2cc854de995f1
Ocipus <106868718+Ocipus@users.noreply.github.com>
2022-07-20 19:29:13 +0200

    Revert "cancelled js fake data forecast"
    This reverts commit 45406a063ff89f781a370cbfcb02e0467f22a5dd.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+let now = new Date();
+let hours = now.getHours();
+if(hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let day = days[now.getDay()];
+
 function formatDay(timestamp) {
 let date = new Date(timestamp * 1000);
 let day = date.getDay();
