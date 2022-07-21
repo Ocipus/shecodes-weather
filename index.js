70602673a027eecab6e492e5cfd76700be13aca3
Ocipus <106868718+Ocipus@users.noreply.github.com>
2022-07-21 23:41:50 +0200

    increased forecast display

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function displayforecast(response) {
   `
         <div class="col-2">
       <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-        <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"width="40">
+        <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"width="42">
         <div class="weather-forecast-temp-min">
          ${Math.round(forecastDay.temp.min)}° 
         <strong class="weather-forecast-temp-min">
