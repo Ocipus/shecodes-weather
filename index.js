7ff5c080ea6848f77fd15ac7ec558e879027ddd8
Ocipus <106868718+Ocipus@users.noreply.github.com>
2022-07-21 23:09:29 +0200

    round temperature test

--- index.js
+++ index.js
@@ -72,8 +72,9 @@ function displayforecast(response) {
   `
         <div class="col-2">
       <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-        <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="40">
-        <div class="weather-forecast-temp-min"> ${forecastDay.temp.max}°<strong class="weather-forecast-temp-min"> ${forecastDay.temp.min}°</strong>
+        <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"width="40">
+        <div class="weather-forecast-temp-min"> {Math.round(forecastDay.temp.max)}° <strong class="weather-forecast-temp-min">
+        {Math.round(forecastDay.temp.max)}° </strong>
         </div>
       </div>
       `;
